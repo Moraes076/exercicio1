8eb1854609e31232124f4d33d014f5995e82d744
Moraes076 <felipe.desmoraes@gmail.com>
2023-06-14 12:09:47 -0300

    Create Exercicio1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let frutas = 'maca'
+
+switch (frutas) {
+case 'maca':
+console.log('não vendemos essa fruta aqui')
+break;
+
+case 'kiwi':
+console.log('estamos com escassez de kiwi')
+break;
+
+case 'melancia':
+console.log ('Aqui está, são 3 reais o quilo')
+break;
+
+default:
+console.log('erro na escolha..')
+break;
+
+}
